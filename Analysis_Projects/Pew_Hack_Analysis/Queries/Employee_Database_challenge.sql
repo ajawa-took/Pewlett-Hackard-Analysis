39c4cbeda593cb74bc9e36b18cabf821f889627e
ajawa-took <ajawa_took@yahoo.com>
2022-11-28 18:17:31 -0800

    deli 2 done

--- Analysis_Projects/Pew_Hack_Analysis/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects/Pew_Hack_Analysis/Queries/Employee_Database_challenge.sql
@@ -39,5 +39,68 @@ from unique_titles
 group by title
 order by ct desc;
 
+-- checking output
 select * from retiring_titles;
-  
+
+-- Deliv 2
+
+-- first try;
+-- from above, know that all duplicates from "join dept_emp" get removed by "current"
+-- may be new duplicates from "join titles"
+select
+ e.emp_no, e.first_name, e.last_name, e.birth_date,
+ d.from_date, d.to_date, t.title
+from employees e
+left join dept_emp d on e.emp_no = d.emp_no
+left join titles t on e.emp_no = t.emp_no
+where (d.to_date = '9999-01-01' )
+ and (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+order by e.emp_no;
+
+-- explore selection of titles in the provided output for 10291, 10476
+
+select * from titles
+where (emp_no = 10291) or (emp_no = 10476);
+
+select * from dept_emp
+where (emp_no = 10291) or (emp_no = 10476);
+
+-- each has only one record in dept_emp
+-- each has 2 records in titles: both joined in 1987 as 'Staff'
+-- and were promoted to 'Senior Staff' in 1994
+-- provided output gives different totles for them; but should give 'senior' to both
+
+-- let's see, do all employees with current dept also have a current title?
+
+select max(ct), count(*) from         -- returns (1, 240,124) no dupes, and 240,124 emp_no
+    ( select emp_no, count(*) as ct
+        from dept_emp 
+        where to_date = '9999-01-01'
+        group by emp_no
+    ) as stuff;
+
+select max(ct), count(*) from         -- returns (1, 240,124): no dupes, same count
+    ( select emp_no, count(*) as ct
+        from titles 
+        where to_date = '9999-01-01'
+        group by emp_no
+    ) as stuff;
+
+-- So, the right thing to do is use to_date from titles,
+-- and then I don't need dept_emp at all - no, they want from_date
+-- and we know that's gonna be different from two emp_no's we looked at above:
+-- their title.from_date (for current title) is in 1994, but for current dept is in 1987
+
+-- second try: just chnage the table from which to_date is taken
+-- AND need to restrict dept_emp's to_date as well, or else dupes return
+select
+ e.emp_no, e.first_name, e.last_name, e.birth_date,
+ d.from_date, t.to_date, t.title
+into mentorship_eligibilty
+from employees e
+left join dept_emp d on e.emp_no = d.emp_no
+left join titles t on e.emp_no = t.emp_no
+where (t.to_date = '9999-01-01' )
+ and (d.to_date = '9999-01-01')
+ and (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+order by e.emp_no;
